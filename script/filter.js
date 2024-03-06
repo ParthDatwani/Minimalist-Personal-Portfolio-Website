02d6a0c41b7ef7a9ad78c3b491fd919bb6bbc5bf
Parth Datwani <53040741+ParthDatwani@users.noreply.github.com>
2024-03-06 17:54:44 +1100

    Add files via upload

---
+++ script/filter.js
@@ -0,0 +1,81 @@
+$(document).ready(function() {
+    // Set 'ALL' filter to active on page load
+    $('.filter-btn[data-filter="all"]').addClass('active');
+    $('.dropdown-contentfilter-dropdown [data-filter="all"]').addClass('active');
+
+    // Function to create a status dot based on the project status
+    function createStatusDot(status) {
+        const color = status === 'active' ? '#66FF00' : status === 'closed' ? '#DD0004' : status === 'work' ? '#E300FC' : '#4169E1';
+        return `<span class="status-dot" style="color: ${color};">●</span>`;
+    }
+
+    // Function to insert projects into the container
+    function displayProjects(projects) {
+        const container = $('#projects-container');
+        container.empty(); // Clear the container
+        
+        projects.forEach(project => {
+            const projectHTML = `
+            <div onclick="location.href='${project.externalLink}';" class="project-item" target="_blank" data-category="${project.toolUsed}" data-link-name="${project.title}">
+                <div class="project-header">
+                    <h4>${project.title}</h4>
+                    ${createStatusDot(project.status)}
+                </div>
+                <p>${project.description}</p>
+                <img src="${project.imagePath}" alt="${project.title}" />
+            </div>
+        `;
+            container.append(projectHTML);
+        });
+    }
+    
+    // Load the data from the JSON file
+    $.getJSON('../json/Project.json', function(data) {
+        displayProjects(data.projects);
+    });
+
+    // Filter projects on button click
+    $('.filter-btn').click(function() {
+        const filter = $(this).data('filter');
+        
+        // Remove 'active' class from all buttons and then add to the clicked one
+        $('.filter-btn').removeClass('active');
+        $(this).addClass('active');
+        
+        // Filter project items
+        if (filter === 'all') {
+            $('.project-item').show();
+        } else {
+            $('.project-item').each(function() {
+                if ($(this).data('category') === filter) {
+                    $(this).show();
+                } else {
+                    $(this).hide();
+                }
+            });
+        }
+    });
+
+    
+    // Filter projects on dropdown clicks
+    $('.filter-dropdown').click(function() {
+        const filter = $(this).data('filter');
+        
+        // Remove 'active' class from all buttons and then add to the clicked one
+        $('.filter-dropdown').removeClass('active');
+        $(this).addClass('active');
+        
+        // Filter project items
+        if (filter === 'all') {
+            $('.project-item').show();
+        } else {
+            $('.project-item').each(function() {
+                if ($(this).data('category') === filter) {
+                    $(this).show();
+                } else {
+                    $(this).hide();
+                }
+            });
+        }
+    });
+});
---
+++ script/script.js
@@ -0,0 +1,14 @@
+// Load the data from the JSON file and update the HTML content
+fetch('../json/profiledata.json')
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('logo1').textContent = data.logo1;
+        document.getElementById('logo2').textContent = data.logo2;
+        document.getElementById('name').textContent = data.name;
+        document.getElementById('title').textContent = data.title;
+        document.getElementById('description').textContent = data.description;
+        document.getElementById('line1').textContent = data.line1;
+        document.getElementById('line2').textContent = data.line2;
+        document.getElementById('line3').textContent = data.line3;
+    })
+    .catch(error => console.error('Error loading data:', error));
